4d54b08f78cc5f159c83c08e8c7e06bf80857cb6
root <root@localhost.localdomain>
2015-10-05 20:14:31 +0000

    Completed Vaidations

---
+++ test/models/chef_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(chefname: 'vishal', email: 'vishal92.p@gmail.com')
+  end
+
+  test 'chef should be valid'do
+    assert @chef.valid?
+  end
+
+  test 'chefname must be present'do
+    @chef.chefname = ' '
+    assert_not @chef.valid?
+
+  end
+
+  test 'chefname should not be loo long' do
+    @chef.chefname = 'a' * 41
+    assert_not @chef.valid?
+  end
+
+  test 'chefname should not be loo short' do
+    @chef.chefname = 'aa'
+    assert_not @chef.valid?
+
+  end
+
+  test 'email must be present'do
+
+    @chef.email = ' '
+    assert_not @chef.valid?
+
+  end
+
+  test 'email length should be within bounds'do
+
+    @chef.email = 'a'*101 + '@example.com'
+    assert_not @chef.valid?
+
+  end
+
+  test 'email should be unique'do
+    dup_chef = @chef.dup
+    dup_chef_email = @chef.email.upcase
+    assert_not dup_chef.valid?
+  end
+
+  test 'eamil must accept valid addresses'do
+
+    valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com
+    first.last@eem.au laura+joe@monk.cm ]
+
+    valid_addresses.each do |va|
+      @chef.email = va
+      assert @chef.valid?, '#{va.inspect} should be valid'
+
+    end
+
+  end
+  test 'eamil must reject invalid addresses'do
+
+    invalid_addresses = %w[user@example,com user_at_eee.org
+    user.name@example.eee@i_am.com foo@ee+aar.com  ]
+
+    invalid_addresses.each do |ia|
+      @chef.email = ia
+      assert_not @chef.valid?, '#{ia.inspect} should be invalid'
+    end
+
+  end
+
+end
