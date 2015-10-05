325af43cedd4801fdece9b7bc75e48d1cf3ede60
root <root@localhost.localdomain>
2015-10-05 19:29:13 +0000

    Validation complete for recipes

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -12,9 +12,65 @@ class Recipe < ActiveSupport::TestCase
     assert @recipe.valid?
   end
 
-  test "name should be valid" do
+  test "name should be present" do
     @recipe.name = " "
     assert_not @recipe.valid?
+  end
+
+  test 'name length should not be too long' do
+
+    @recipe.name = 'a' * 101
+    assert_not @recipe.valid?
+
+  end
+
+  test 'name length should not be too short' do
+
+    @recipe.name = 'aaaaa'
+    assert_not @recipe.valid?
+
+  end
+
+  test 'summary should be present' do
+
+    @recipe.summary = ' '
+    assert_not @recipe.valid?
+
+  end
+
+  test 'summary length should not be too long' do
+
+    @recipe.summary = 'a' * 151
+    assert_not @recipe.valid?
+
+  end
+
+  test 'summary length should not be too short' do
+
+    @recipe.summary = 'a' * 9
+    assert_not @recipe.valid?
+
+  end
+
+  test 'description must be present' do
+
+    @recipe.description = ' '
+    assert_not @recipe.valid?
+
+  end
+
+  test 'description length should not be too long' do
+
+    @recipe.description = 'a' * 501
+    assert_not @recipe.valid?
+
+  end
+
+  test 'description length should not be too short' do
+
+    @recipe.description = 'a' * 19
+    assert_not @recipe.valid?
+
   end
 
 end
