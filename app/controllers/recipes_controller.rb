a82fbf5c9d550b9d62cd90ab13e2e835141c3adf
vishal-patel17 <vishal92.p@gmail.com>
2015-10-07 16:50:05 +0000

    added Partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,6 +33,25 @@ class RecipesController < ApplicationController
 
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = 'Your recipe was updated successfully'
+      redirect_to recipe_path(@recipe)
+
+
+    else
+      render :edit
+
+    end
+
+  end
+
   private
 
     def recipe_params
